794d7af2080524420bcf7671f2583b5e50ced937
Louis Bima Susanto <115883367+LouisBimaSusanto@users.noreply.github.com>
2025-03-06 23:38:01 +0700

    Update
    - Pacman bisa gerak

--- Visual Code_Pacman/Pacman.h
+++ Visual Code_Pacman/Pacman.h
@@ -1,14 +1,13 @@
-// Header Pacman
-
 #ifndef PACMAN_H
 #define PACMAN_H
+#include "Map.h"
 
 class Pacman
 {
 public:
-    int x, y; // Position Pacman
+    int x, y;
     Pacman(int startX, int startY);
-    void move(int dx, int dy);
+    void move(int dx, int dy, Map &gameMap);
 };
 
 #endif
